206b8b6b700c5f46b3ade4edabd7c8ca00afc942
StoneSteel27 <stonesteel27@gmail.com>
2024-03-15 19:35:00 +0530

    Minor Linting Fixes

--- FlowQ/cluster/__main__.py
+++ FlowQ/cluster/__main__.py
@@ -4,8 +4,7 @@ import uuid
 from .FlowQluster import FlowQluster
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog="python -m FlowQ.cluster"
-                                     , description='Initialize FlowQ cluster')
+    parser = argparse.ArgumentParser(prog="python -m FlowQ.cluster", description='Initialize FlowQ cluster')
     parser.add_argument('-c', '--channel', required=True, type=str, help='Communication Channel Name')
     args = parser.parse_args()
     flow = FlowQluster(args.channel)
